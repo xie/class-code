c3bb30fda955f855ce895bdaed303c67cbfbfeb1
hughxie <hughxie@outlook.com>
2017-12-05 20:13:17 -0500

    working singlePrime

--- COMP2401/a5/A5/singlePrime.c
+++ COMP2401/a5/A5/singlePrime.c
@@ -11,22 +11,34 @@ int morph(char *number);
 int main(int argc, char* argv[]) {
 
   //check if user input filename is not null
-
-
-
   if (argv[1] != NULL) {
 
     FILE * file;
-
     //define file
     file = fopen(argv[1], "r");
     //check if file exists and can be opened
     if (file) {
       //confirmation output
       printf("%s\n", "File exists.");
-      //send filename to moprh method
-      morph(argv[1]);
-      fclose(file);
+
+      //var for int from file
+      unsigned int num;
+      //read from binary file and check if successful
+      if (fread(&num, sizeof(int), 1, file) != 1) {
+        printf("%s\n", "failed to read from binary file.");
+      } else {
+        printf("%s\n", "read binary file success");
+
+        //char array to hold number as string
+        char buffer[30];
+        //store value of num in buffer
+        sprintf(buffer,"%d\n", num);
+
+
+        printf("%s\n", "sending to morph");
+        //send filename to moprh method
+        morph(buffer);
+      }
     } else {
       //error message
       printf("%s, %s, %s \n", "Error: file ", argv[1], " does not exist.");
@@ -51,7 +63,7 @@ int morph(char *number) {
   char *params[3] = {"isPrime", number};
   int r = execvp(name,params);
 
-  printf("%s, %d\n","result: ", r);
+  printf("%d\n",r);
 
   return 1;
 
