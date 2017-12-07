1d4c7355342bdbd18ce12da756f3e391d76697c2
hughxie <hughxie@outlook.com>
2017-12-06 19:31:31 -0500

    bonus working

--- 2401/a5/isPrime.c
+++ 2401/a5/isPrime.c
@@ -45,15 +45,7 @@ int main(int argc, char *argv[])
 
 		if (n > 0) {
 			int r = isPrime(n);
-<<<<<<< HEAD
-<<<<<<< HEAD
 			//printf("The number %d is: %d\n", n, r);
-=======
-			// printf("The number %d is: %d\n", n, r);
->>>>>>> 5e1e56b4c0696776700f0276ee80870080c00093
-=======
-			// printf("The number %d is: %d\n", n, r);
->>>>>>> 5e1e56b4c0696776700f0276ee80870080c00093
 			return r;
 		} else {
 			return 2;
@@ -83,26 +75,10 @@ int isPrime(unsigned int number)
 	for(i = 2; i*i <= number; i++) {
 		usleep(100);
 		if (number % i == 0) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 			//printf("%d is not a prime number\n",number);
 			return(0);
 		}
 	}
     //printf("%d is a prime number\n",number);
-=======
-			// printf("%d is not a prime number\n",number);
-			return(0);
-		}
-	}
-    // printf("%d is a prime number\n",number);
->>>>>>> 5e1e56b4c0696776700f0276ee80870080c00093
-=======
-			// printf("%d is not a prime number\n",number);
-			return(0);
-		}
-	}
-    // printf("%d is a prime number\n",number);
->>>>>>> 5e1e56b4c0696776700f0276ee80870080c00093
 	return(1);
 }
---
+++ 2401/a5/multiSpawnSignal.h
@@ -0,0 +1,9 @@
+int getFileSize(char *name);
+
+void printArr(int arr[], int len);
+
+void my_handler(int signum);
+
+void spawn(int arr[], int len);
+
+int morph(char *number);
